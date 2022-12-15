daf677e96747b82d2b3577bc701ced9bdeaa6389
Mike-cbs <78037691+Mike-cbs@users.noreply.github.com>
2022-12-14 17:27:31 -0800

    main

--- Abacus/Program.cs
+++ Abacus/Program.cs
@@ -16,6 +16,7 @@ namespace Abacus
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Main");
         }
     }
 }
