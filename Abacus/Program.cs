642d658e72ec5820740543d52dd37bf30a617139
Mike-cbs <78037691+Mike-cbs@users.noreply.github.com>
2022-12-14 16:51:16 -0800

    // Test azeem

--- Abacus/Program.cs
+++ Abacus/Program.cs
@@ -16,6 +16,8 @@ namespace Abacus
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.WriteLine("Test Azeem");
         }
     }
 }
