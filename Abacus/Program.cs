7fdd7b6ebef53b3aac40a2bd76b415152f354825
Mike-cbs <78037691+Mike-cbs@users.noreply.github.com>
2022-12-14 17:14:01 -0800

    980

--- Abacus/Program.cs
+++ Abacus/Program.cs
@@ -17,7 +17,7 @@ namespace Abacus
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            Console.WriteLine("TEST");
+            Console.WriteLine("TEST 980");
         }
     }
 }
